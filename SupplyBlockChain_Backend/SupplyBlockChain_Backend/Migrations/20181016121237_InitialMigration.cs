6c2a150bd206f0d09fb0c24d0ea9ee376f630f9d
Sudesh Kumar <sudesh1611@gmail.com>
2018-10-16 18:00:17 +0530

    Removed Unnecessary Pages , Added Database

---
+++ SupplyBlockChain_Backend/SupplyBlockChain_Backend/Migrations/20181016121237_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SupplyBlockChain_Backend.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserAccounts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserName = table.Column<string>(nullable: false),
+                    FullName = table.Column<string>(nullable: false),
+                    EmailID = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    ConfirmPassword = table.Column<string>(nullable: false),
+                    AccessRights = table.Column<string>(nullable: true),
+                    AssociatedProductTypes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccounts", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAccounts");
+        }
+    }
+}
